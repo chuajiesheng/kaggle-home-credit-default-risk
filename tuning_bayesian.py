cf365a35898e61ccb5e2ef91a5dde76d7f106518
Chua Jie Sheng <chuajiesheng@gmail.com>
2018-07-04 01:37:34 +0800

    Update bayesian tuning with new feature set

--- tuning_bayesian.py
+++ tuning_bayesian.py
@@ -17,41 +17,35 @@ n_threads = n_jobs = round(cpu_count() * 2 * 0.75)
 n_jobs = cpu_count()
 verbose = 1
 
-X, y, X_test, train_test, train_test_one_hot, bureau, bureau_bal, prev, credit_card_bal, pos_cash, installment_payment = f.read_dataset()
-feature_mapping = f.get_feature_mapping(train_test, train_test_one_hot, bureau, bureau_bal, prev, credit_card_bal, pos_cash, installment_payment)
-features = Parallel(n_jobs=n_jobs, verbose=verbose)(feature_mapping)
+features = Parallel(n_jobs=cpu_count())(f.get_selected_features_df())
 
-for df in features:
-    X = X.merge(right=df, how='left', on=ID_COLUMN)
-    X_test = X_test.merge(right=df, how='left', on=ID_COLUMN)
-    assert X.shape[0] == 307511
+df = None
+for i in range(len(features)):
+    print('at {} of {}'.format(i, len(features)))
+    feature = features[i]
+    df = df.join(feature, how='left', on='SK_ID_CURR') if df is not None else feature
 
-print('X.shape', X.shape)
-print('X_test.shape', X_test.shape)
-
-#
-# Delete customer Id
-#
-
-del X['SK_ID_CURR']
-del X_test['SK_ID_CURR']
+print(df.shape)
+df.drop(f.columns_not_needed(), axis=1, inplace=True, errors='ignore')
 
 #
 # Prepare data
 #
+train_df = df[df[LABEL_COLUMN].notnull()]
+feats = [f for f in train_df.columns if f not in [LABEL_COLUMN, ID_COLUMN, 'SK_ID_BUREAU', 'SK_ID_PREV', 'index']]
+data = lgb.Dataset(data=train_df[feats], label=train_df[LABEL_COLUMN], free_raw_data=False, silent=True)
 
-data = lgb.Dataset(X, label=y)
-
-num_rounds = 3000
+num_rounds = 10000
 random_state = 42
 num_iter = 1000
 init_points = 5
 params = {
-    'learning_rate': 0.1,
+    'learning_rate': 0.02,
     'verbose_eval': True,
     'seed': random_state,
     'nthread': n_threads,
     'boosting': 'gbdt',
+    'objective': 'binary',
     'metric': 'auc',
     'seed': 42,
     'is_unbalance': True,
@@ -77,7 +71,7 @@ def lgb_evaluate(max_bin, min_child_samples, learning_rate, num_leaves, min_chil
                        data,
                        nfold=5,
                        num_boost_round=num_rounds,
-                       early_stopping_rounds=50,
+                       early_stopping_rounds=200,
                        seed=random_state,
                        show_stdv=True)
 
@@ -88,12 +82,12 @@ lgbBO = BayesianOptimization(lgb_evaluate, {'max_bin': (2**3, 2**11),
                                             'min_child_samples': (10, 50),
                                             'learning_rate': (0.001, 0.5),
                                             'num_leaves': (2, 2 ** 10),
-                                            'min_child_weight': (1, 22),
+                                            'min_child_weight': (1, 60),
                                             'colsample_bytree': (0.2, 1),
                                             'max_depth': (5, 15),
                                             'subsample': (0.2, 1),
-                                            'reg_lambda': (0, 100),
-                                            'reg_alpha': (0, 100),
+                                            'reg_lambda': (0, 10),
+                                            'reg_alpha': (0, 10),
                                             'min_split_gain': (0.01, 0.1),
                                             })
 
