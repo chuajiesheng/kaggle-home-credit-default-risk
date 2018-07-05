b3b8b9561e1ca2ae6e1b5e3c4c017c450a478a66
Chua Jie Sheng <chuajiesheng@gmail.com>
2018-07-05 15:16:54 +0800

    Search for a good random_state also

--- tuning_bayesian.py
+++ tuning_bayesian.py
@@ -13,7 +13,7 @@ warnings.filterwarnings('ignore')
 ID_COLUMN = 'SK_ID_CURR'
 LABEL_COLUMN = 'TARGET'
 
-n_threads = n_jobs = round(cpu_count() * 2 * 0.75)
+n_threads = n_jobs = round(cpu_count() * 2 * 0.85)
 n_jobs = cpu_count()
 verbose = 1
 
@@ -36,24 +36,20 @@ feats = [f for f in train_df.columns if f not in [LABEL_COLUMN, ID_COLUMN, 'SK_I
 data = lgb.Dataset(data=train_df[feats], label=train_df[LABEL_COLUMN], free_raw_data=False, silent=True)
 
 num_rounds = 10000
-random_state = 42
 num_iter = 1000
 init_points = 5
 params = {
     'learning_rate': 0.02,
     'verbose_eval': True,
-    'seed': random_state,
     'nthread': n_threads,
     'boosting': 'gbdt',
     'objective': 'binary',
     'metric': 'auc',
-    'seed': 42,
-    'is_unbalance': True,
-    'device': 'cpu'
+    'device': 'cpu',
 }
 
 
-def lgb_evaluate(max_bin, min_child_samples, learning_rate, num_leaves, min_child_weight, colsample_bytree, max_depth, subsample, reg_lambda, reg_alpha, min_split_gain):
+def lgb_evaluate(max_bin, min_child_samples, learning_rate, num_leaves, min_child_weight, colsample_bytree, max_depth, subsample, reg_lambda, reg_alpha, min_split_gain, random_state):
 
     params['max_bin'] = int(max(max_bin, 1))
     params['min_child_samples'] = int(max(min_child_samples, 0))
@@ -72,7 +68,7 @@ def lgb_evaluate(max_bin, min_child_samples, learning_rate, num_leaves, min_chil
                        nfold=5,
                        num_boost_round=num_rounds,
                        early_stopping_rounds=200,
-                       seed=random_state,
+                       seed=int(random_state),
                        show_stdv=True)
 
     return max(cv_result['auc-mean'])
@@ -89,6 +85,7 @@ lgbBO = BayesianOptimization(lgb_evaluate, {'max_bin': (2**3, 2**11),
                                             'reg_lambda': (0, 10),
                                             'reg_alpha': (0, 10),
                                             'min_split_gain': (0.01, 0.1),
+                                            'random_state': (0, 100000),
                                             })
 
 lgbBO.maximize(init_points=init_points, n_iter=num_iter, acq="poi", xi=0.1)
